dea4a7fb68d8bb486b7cc6f02c2e4a219458bd68
Juan Valdez <juansebastianvaldez99@gmail.com>
2022-03-08 12:24:32 -0300

    responsive terminado

---
+++ app.js
@@ -0,0 +1,12 @@
+const iconoMenu = document.querySelector(".icono-menu");
+const headerNav = document.querySelector(".header__nav");
+
+iconoMenu.addEventListener("click", () => {
+  headerNav.classList.toggle("header__nav__visible");
+
+  if (headerNav.classList.contains("header__nav__visible ")) {
+    iconoMenu.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    iconoMenu.setAttribute("aria-label", "Abrir menú");
+  }
+});
